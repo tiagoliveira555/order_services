8e213bb2df403a9a3f208c16807c704c103fd75b
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-08-26 23:53:13 -0300

    implements method show in user controller

--- app/Controllers/Users.php
+++ app/Controllers/Users.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\UserModel;
+use CodeIgniter\Exceptions\PageNotFoundException;
 
 class Users extends BaseController
 {
@@ -36,12 +37,32 @@ class Users extends BaseController
         foreach ($users as $user) {
             $data[] = [
                 'image' => $user->image,
-                'name' => esc($user->name),
+                'name' => anchor("/users/show/{$user->id}", esc($user->name), 'title: "exibir usuário '. esc($user->name).'"'),
                 'email' => esc($user->email),
                 'active' => $user->active === 't' ? '<i class="fa fa-unlock text-success"></i>&nbsp;Ativo' : '<i class="fa fa-lock text-warning"></i>&nbsp;Inativo'
             ];
         }
 
         return $this->response->setJSON(['data' => $data]);
+    }
+
+    public function show(int $id = null)
+    {
+        $user = $this->getUserOr404($id);
+        
+        $data = [
+            'title' => 'Detalhando o usuário ' . esc($user->name),
+            'user' => $user
+        ];
+
+        return view('Users/show', $data);
+    }
+
+    private function getUserOr404(int $id = null)
+    {
+        if(!$id || !$user = $this->userModel->withDeleted(true)->find($id)) {
+            return throw PageNotFoundException::forPageNotFound("Não encontramos o usuário com o id {$id}");
+        }
+        return $user;
     }
 }
